d7260896d9bdda40946902a4dfb3848c34fb1b74
eap85 <eprice122@gmail.com>
2020-02-26 12:42:14 -0500

    Are you guys reading these

---
+++ TrainSystem/src/main.java
@@ -0,0 +1,16 @@
+package trainGame;
+import trainGame.track.TrackModule;
+
+class Main { 
+    public static void main(String args[])
+    { 
+        try {
+            TrackModule tm= new TrackModule();
+            tm.userInterface();
+            int x=0;
+        }
+        catch( Exception e) {
+
+        }
+    } 
+} 
