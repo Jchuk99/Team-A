83dd2fb55569853e995218cbf5353135514bd646
plasma119 <leechikwok@gmail.com>
2020-02-26 02:48:13 -0500

    UI layout finished

--- TrainSystem/src/TrainModel/TrainModelUI.java
+++ TrainSystem/src/TrainModel/TrainModelUI.java
@@ -44,33 +44,44 @@ public class TrainModelUI extends Application {
     public void start(final Stage primaryStage) {
         primaryStage.setTitle("TrainModel UI");
 
+        /****** select train ******/
         // TODO: get train data from module
         ObservableList<Person> trainData;
         trainData = FXCollections.observableArrayList();
 
-        /****** select train ******/
         final TableView trainTable = createTrainTable(trainData);
+        trainTable.setPrefWidth(width / 8);
 
         // testing: add a train to test
         trainData.add(new Person("Train 1"));
         /****** select train ******/
+
         /****** beacon ******/
+        final HBox beaconBox = new HBox(10, createTextBox("Beacon"), createLabelBox("STATION; CASTLE SHANNON; BLOCK 96;"));
+        beaconBox.setStyle("-fx-border-style: solid inside; -fx-border-width: 1; -fx-padding: 10;");
         /****** beacon ******/
+
         /****** top box - time and signal ******/
         final HBox topBox = createTopBox();
+        topBox.setStyle("-fx-border-style: solid inside; -fx-border-width: 1; -fx-padding: 10;");
         /****** top box - time and signal ******/
+
         /****** info ******/
         final HBox infoBox = createInfoBox();
+        infoBox.setStyle("-fx-border-style: solid inside; -fx-border-width: 1; -fx-padding: 10;");
         /****** info ******/
+
         /****** status ******/
+        final VBox statusBox = createStatusBox();
+        statusBox.setStyle("-fx-border-style: solid inside; -fx-border-width: 1; -fx-padding: 10;");
         /****** status ******/
 
         // combining boxes
         final VBox box1 = new VBox(10, topBox, infoBox);
         final HBox box2 = new HBox(10, trainTable, box1);
+        final VBox box3 = new VBox(10, box2, beaconBox);
 
-
-        final HBox topHalf = new HBox(10, box2);
+        final HBox topHalf = new HBox(10, box3, statusBox);
         final TableView mapTable = new TableView();
 
         topHalf.setPrefHeight(height / 2);
@@ -88,7 +99,6 @@ public class TrainModelUI extends Application {
     private TableView createTrainTable(ObservableList<Person> item) {
         final TableView trainTable = new TableView();
         trainTable.setPlaceholder(new Label("No trains available"));
-        trainTable.setPrefWidth(width / 6);
 
         final TableColumn<String, Person> trainTable_col = new TableColumn<>("Select Train");
         trainTable_col.setCellValueFactory(new PropertyValueFactory<>("firstName"));
@@ -100,36 +110,173 @@ public class TrainModelUI extends Application {
     }
 
     private HBox createTopBox() {
-        final HBox topBox = new HBox(10);
+        VBox time = createLabelBox("11:00:23 am");
+        final HBox timeBox = new HBox(10, createTextBox("Time"), time);
+
+        Circle circleG = createCircle(10, Color.GREEN);
+        Circle circleY = createCircle(10, Color.YELLOW);
+        Circle circleR = createCircle(10, Color.RED);
+        final HBox signalBox = new HBox(10, circleG, circleY, circleR);
+        signalBox.setAlignment(Pos.CENTER);
+        signalBox.setStyle("-fx-border-style: solid inside; -fx-border-width: 2; -fx-padding: 10;");
+
+        final HBox topBox = new HBox(10, createHSpacer(), timeBox, createHSpacer(), signalBox, createHSpacer());
         return topBox;
     }
 
     private HBox createInfoBox() {
         // TODO: pull data from module
-        HBox leftBox1 = createDataField("Suggested Speed", "30 mph");
-        HBox leftBox2 = createDataField("Current Speed", "30 mph");
-        HBox leftBox3 = createDataField("Authority", "1000 ft");
-        HBox leftBox4 = createDataField("Engine Power", "100 kW");
+        VBox name1 = createTextBox("Suggested Speed");
+        VBox name2 = createTextBox("Current Speed");
+        VBox name3 = createTextBox("Authority");
+        VBox name4 = createTextBox("Engine Power");
+        VBox nameBox1 = new VBox(10, name1, name2, name3, name4);
 
-        HBox rightBox1 = createDataField("Passenger Count", "150");
-        HBox rightBox2 = createDataField("Current Weight", "52.2 tons");
-        HBox rightBox3 = createDataField("Current Acceleration", "0.44 m/s^2");
-        HBox rightBox4 = createDataField("Current Grade", "0.5 %");
-        HBox rightBox5 = createDataField("Temperature Inside", "70 F");
+        VBox name5 = createTextBox("Passenger Count");
+        VBox name6 = createTextBox("Current Weight");
+        VBox name7 = createTextBox("Current Acceleration");
+        VBox name8 = createTextBox("Current Grade");
+        VBox name9 = createTextBox("Temperature Inside");
+        VBox nameBox2 = new VBox(10, name5, name6, name7, name8, name9);
 
-        VBox leftBox = new VBox(10, leftBox1, leftBox2, leftBox3, leftBox4);
-        VBox rightBox = new VBox(10, rightBox1, rightBox2, rightBox3, rightBox4, rightBox5);
-        final HBox infoBox = new HBox(10, leftBox, rightBox);
+        VBox label1 = createLabelBox("30 mph");
+        VBox label2 = createLabelBox("30 mph");
+        VBox label3 = createLabelBox("1000 ft");
+        VBox label4 = createLabelBox("100 kW");
+        VBox labelBox1 = new VBox(10, label1, label2, label3, label4);
+
+        VBox label5 = createLabelBox("150");
+        VBox label6 = createLabelBox("52.2 tons");
+        VBox label7 = createLabelBox("0.44 m/s^2");
+        VBox label8 = createLabelBox("0.5 %");
+        VBox label9 = createLabelBox("70 F");
+        VBox labelBox2 = new VBox(10, label5, label6, label7, label8, label9);
+        
+        final HBox infoBox = new HBox(10, createHSpacer(), nameBox1, labelBox1, createHSpacer(), nameBox2, labelBox2, createHSpacer());
         return infoBox;
     }
 
-    private HBox createDataField(String text, String label) {
-        Text textbox = new Text(text);
-        Label labelbox = new Label(label);
-        labelbox.setStyle("-fx-border-style: solid inside;" + "-fx-border-width: 2;" + "-fx-padding: 5;");
-        HBox box = new HBox(10, textbox, createHSpacer(), labelbox);
+    private VBox createStatusBox() {
+        VBox name1 = createTextBox("Left Door");
+        VBox name2 = createTextBox("Right Door");
+        VBox name3 = createTextBox("Lights");
+        VBox name4 = createTextBox("Service Brake");
+        VBox name5 = createTextBox("Emergency Brake");
+        VBox name6 = createTextBox("Train Engine");
+        VBox nameBox = new VBox(10, createVBox(), name1, name2, name3, name4, name5, name6);
+
+        VBox light1 = createCircleBox(10, Color.GREEN);
+        VBox light2 = createCircleBox(10, Color.GREEN);
+        VBox light3 = createCircleBox(10, Color.GREEN);
+        VBox light4 = createCircleBox(10, Color.GREEN);
+        VBox light5 = createCircleBox(10, Color.GREEN);
+        VBox light6 = createCircleBox(10, Color.GREEN);
+        VBox lightBox = new VBox(10, createTextBox("Status"), light1, light2, light3, light4, light5, light6);
+
+        VBox button1 = createButtonBox("Insert Failure", 100, 30);
+        VBox button2 = createButtonBox("Insert Failure", 100, 30);
+        VBox button3 = createButtonBox("Insert Failure", 100, 30);
+        VBox button4 = createButtonBox("Insert Failure", 100, 30);
+        VBox button5 = createButtonBox("Insert Failure", 100, 30);
+        VBox button6 = createButtonBox("Insert Failure", 100, 30);
+        VBox buttonBox = new VBox(10, createTextBox("Insert Failure"), button1, button2, button3, button4, button5, button6);
+
+        HBox box = new HBox(10, createHSpacer(), nameBox, createHSpacer(), lightBox, createHSpacer(), buttonBox, createHSpacer());
+
+        final Button ebrakeButton = createButton("Emergency brake", 200, 50);
+        ebrakeButton.setStyle("-fx-border-color: black; -fx-border-width: 2; -fx-font-size:20; -fx-text-fill: black; -fx-background-color: red;");
+        ebrakeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO: button handle
+            }
+        });
+        // TODO: add effect when button is clicked
+        DropShadow shadow = new DropShadow();
+        //Adding the shadow when the mouse cursor is on
+        ebrakeButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+            @Override 
+            public void handle(MouseEvent e) {
+                ebrakeButton.setEffect(shadow);
+            }
+        });
+        //Removing the shadow when the mouse cursor is off
+        ebrakeButton.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+            @Override 
+            public void handle(MouseEvent e) {
+                ebrakeButton.setEffect(null);
+            }
+        });
+
+        final VBox statusBox = new VBox(10, box, ebrakeButton);
+        statusBox.setAlignment(Pos.CENTER);
+        return statusBox;
+    }
+
+    private VBox createVBox() {
+        // all VBox create function are unified to 40px height
+        VBox box = new VBox(0);
+        box.setPrefHeight(40);
         box.setAlignment(Pos.CENTER_LEFT);
         return box;
+    }
+
+    private Button createButton(String text, int width, int height) {
+        Button button = new Button();
+        button.setText(text);
+        button.setPrefWidth(width);
+        button.setPrefHeight(height);
+        return button;
+    }
+
+    private VBox createButtonBox(String text, int width, int height) {
+        // all VBox create function are unified to 40px height
+        VBox buttonBox = new VBox(0, createButton(text, width, height));
+        buttonBox.setPrefHeight(40);
+        buttonBox.setAlignment(Pos.CENTER_LEFT);
+        return buttonBox;
+    }
+
+    private Text createText(String text) {
+        Text t = new Text(text);
+        return t;
+    }
+
+    private VBox createTextBox(String text) {
+        // all VBox create function are unified to 40px height
+        VBox textBox = new VBox(0, createText(text));
+        textBox.setPrefHeight(40);
+        textBox.setAlignment(Pos.CENTER_LEFT);
+        return textBox;
+    }
+
+    private Label createLabel(String text) {
+        Label label = new Label(text);
+        label.setStyle("-fx-border-style: solid inside; -fx-border-width: 2; -fx-padding: 5;");
+        return label;
+    }
+
+    private VBox createLabelBox(String text) {
+        // all VBox create function are unified to 40px height
+        VBox labelBox = new VBox(0, createLabel(text));
+        labelBox.setPrefHeight(40);
+        labelBox.setAlignment(Pos.CENTER_LEFT);
+        return labelBox;
+    }
+
+    private Circle createCircle(int radius, Color color) {
+        Circle circle = new Circle();
+        circle.setRadius(radius);
+        circle.setFill(color);
+        return circle;
+    }
+
+    private VBox createCircleBox(int radius, Color color) {
+        // all VBox create function are unified to 40px height
+        VBox circleBox = new VBox(createCircle(radius, color));
+        circleBox.setPrefHeight(40);
+        circleBox.setAlignment(Pos.CENTER_LEFT);
+        return circleBox;
     }
 
     private static Node createVSpacer() {
