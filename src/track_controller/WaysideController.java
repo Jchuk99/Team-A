beba1fb838ca1392b1ec89e0aa3b3be47d3e261b
Calvin <cdy3870@gmail.com>
2020-02-26 16:40:42 -0500

    fix

---
+++ src/track_controller/WaysideController.java
@@ -0,0 +1,50 @@
+public class WaysideController{
+
+	//private LinkedList<Block> blocks;
+	//private LinkedList<Train> trains;
+	private int id;
+	private PLC plc;
+
+
+	public WaysideController(){
+
+	}
+
+
+	/*public void addBlock(Block block){
+		
+	}*/
+
+	private void uploadPLC(){
+		plc = new PLC("PLC 1");
+
+	}
+}
+/*private void setTrains()
+private LinkedList<Block> getBlockJurisdiction(){
+	return blocks
+}
+private void setBlockJurisdiction(LinkedList<Block> blocks){
+	this.blocks = blocks
+}
+private void toggleSwitch(LinkedList<Block>){
+	plc.runPLC()
+}
+private void closeBlock(LinkedList<Block>){
+	plc.runPLC()
+}
+private void openGate(LinkedList<Block>){
+	plc.runPLC()
+}
+private void activateLights(LinkedList<Block>){
+	plc.runPLC()
+}
+private void setSuggestedSpeed(LinkedList<Block>){
+	plc.runPLC()
+}
+private void setAuthority(LinkedList<Block>){
+	plc.runPLC()
+}
+private LinkedList<Block> getBlockInfo(LinkedList<Block>){
+private
+}*/
