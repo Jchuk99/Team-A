2da8dff38830e737db1623a9c1609dfa1493d483
Calvin <cdy3870@gmail.com>
2020-04-12 20:40:22 -0400

    getters for track

--- src/track_controller/TrackControllerModule.java
+++ src/track_controller/TrackControllerModule.java
@@ -34,11 +34,10 @@ public class TrackControllerModule extends Module {
 
 	@Override
 	public void update(){
-		trains = this.ctcModule.getTrains();
-		/*
+		trains = this.ctcModule.getTrains();	
 		switchPositions = this.ctcModule.getSwitchPositions();
 		closedBlocks = this.ctcModule.getClosedBlocks();
-		for(WaysideController waysideController : waysideControllers){
+		/*for(WaysideController waysideController : waysideControllers){
 			Set<CTCTrain> trainsInJuris = getTrainsInJuris(waysideController.getBlocks());
 			HashMap<UUID, position> switchesInJuris = getSwitchesInJuris(waysideController.getBlocks());
 			ArrayList<Block> closedBlocksInJuris = getClosedBlocksInJuris(waysideController.getBlocks());
