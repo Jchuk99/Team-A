be740f12fa39c11b67697f1fa080420178fbe290
JChuk99 <jochuk95@gmail.com>
2020-04-22 18:43:08 -0400

    iteration 3 should be done

--- src/ctc/CTCModule.java
+++ src/ctc/CTCModule.java
@@ -18,7 +18,7 @@ import src.track_module.Block;
 import src.track_module.Edge;
 
 public class CTCModule extends Module{
-    public static final int MAX_AUTHORITY = 5;
+    public static final int MAX_AUTHORITY = 4;
     public static CTCMap map = null;
     public StringProperty greenTickets = new SimpleStringProperty("");
     public StringProperty redTickets = new SimpleStringProperty("");
